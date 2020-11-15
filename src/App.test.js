200a73b22af62e676066380817c0e8af2439721c
Josh Wood <gdjoshwood@gmail.com>
2020-11-15 15:01:43 -0800

    add some simple tests to ensure business logic works

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react';
+import { render, waitFor, fireEvent, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Note Manager/i);
+  expect(headerElement).toBeInTheDocument();
+});
+
+const getChangedNewNoteInput = () => {
+  const inputElement = screen.getByPlaceholderText('Jot down your thoughts...');
+  fireEvent.change(
+    inputElement,
+    {
+      bubbles: true,
+      cancelable: true,
+      target: { value: 'remind me'}
+    }
+  )
+  return inputElement;
+}
+test('changed new note text', () => {
+  render(<App />);
+  const inputElement = getChangedNewNoteInput();
+  expect(inputElement.value).toBe('remind me')
+});
+
+test('successfully created a note', () => {
+  render(<App />);
+
+  const inputElement = getChangedNewNoteInput();
+  fireEvent(
+    screen.getByText('Create'),
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+    })
+  )
+  const noteItem = screen.getByText(/remind me/i);
+  expect(noteItem).toBeInTheDocument();
 });
